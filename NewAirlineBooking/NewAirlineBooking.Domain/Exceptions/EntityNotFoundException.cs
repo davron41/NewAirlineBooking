7de37d755cee568718307b608f6d9cb01e30465f
Davronbek <davronbek8733@gmail.com>
2025-01-12 01:26:27 +0500

    create exceptions

---
+++ NewAirlineBooking/NewAirlineBooking.Domain/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,6 @@
+﻿namespace NewAirlineBooking.Domain.Exceptions;
+public class EntityNotFoundException : ApplicationException
+{
+    public EntityNotFoundException() : base() { }
+    public EntityNotFoundException(string message) : base(message) { }
+}
