4a165f5815e89f5e231d5743eeb59d649f7cac2a
Davronbek <davronbek8733@gmail.com>
2025-01-12 01:24:54 +0500

    Initial project setup

---
+++ NewAirlineBooking/NewAirlineBooking.Infrastructure/Repositories/PassengerRepository.cs
@@ -0,0 +1,11 @@
+﻿using NewAirlineBooking.Domain.Entities;
+using NewAirlineBooking.Domain.Interfaces;
+using NewAirlineBooking.Infrastructure.Persistence;
+
+namespace NewAirlineBooking.Infrastructure.Repositories;
+internal sealed class PassengerRepository : RepositoryBase<Passenger>, IPassengerRepository
+{
+    public PassengerRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
