a6cf1a44357028a310d0df4cec97bf3d8b9610d1
Davronbek <davronbek8733@gmail.com>
2025-01-12 02:40:24 +0500

    Add dependency injection

---
+++ NewAirlineBooking/NewAirlineBooking.Infrastructure/Extensions/DependencyInjection.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NewAirlineBooking.Domain.Interfaces;
+using NewAirlineBooking.Infrastructure.Persistence;
+using NewAirlineBooking.Infrastructure.Repositories;
+
+namespace NewAirlineBooking.Infrastructure.Extensions;
+public static class DependencyInjection
+{
+    public static IServiceCollection RegisterInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IPassengerRepository, PassengerRepository>();
+        services.AddScoped<IFlightRepository, FlightRepository>();
+        services.AddScoped<ITicketRepository, TicketRepository>();
+        services.AddScoped<IBookingRepository, BookingRepository>();
+        services.AddScoped<IBookingRepository, BookingRepository>();
+        services.AddScoped<ICommonRepository, CommonRepository>();
+
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+
+
+        services
+            .AddIdentity<IdentityUser<Guid>, IdentityRole<Guid>>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = true;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequiredLength = 8;
+
+                options.Lockout.AllowedForNewUsers = true;
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+
+                options.User.RequireUniqueEmail = true;
+            })
+              .AddEntityFrameworkStores<ApplicationDbContext>()
+              .AddDefaultTokenProviders();
+
+        return services;
+
+
+    }
+}
