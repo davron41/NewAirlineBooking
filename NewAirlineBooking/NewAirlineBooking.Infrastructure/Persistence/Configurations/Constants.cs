5b642fa6be5aea844c247bd4ed090c9316bbf233
Davronbek <davronbek8733@gmail.com>
2025-01-12 02:41:44 +0500

    add constants

---
+++ NewAirlineBooking/NewAirlineBooking.Infrastructure/Persistence/Configurations/Constants.cs
@@ -0,0 +1,6 @@
+﻿namespace NewAirlineBooking.Infrastructure.Persistence.Configurations;
+public static class Constants
+{
+    public const int DEFAULT_STRING_LENGTH = 255;
+    public const int MAX_STRING_LENGTH = 500;
+}
