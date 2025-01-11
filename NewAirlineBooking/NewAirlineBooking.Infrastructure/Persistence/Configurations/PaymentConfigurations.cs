1a95822ed25887a8699ab980b87a35b62c74dc1d
Davronbek <davronbek8733@gmail.com>
2025-01-12 02:42:12 +0500

    update configurations

---
+++ NewAirlineBooking/NewAirlineBooking.Infrastructure/Persistence/Configurations/PaymentConfigurations.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NewAirlineBooking.Domain.Entities;
+
+namespace NewAirlineBooking.Infrastructure.Persistence.Configurations;
+public class PaymentConfigurations : IEntityTypeConfiguration<Payment>
+{
+    public void Configure(EntityTypeBuilder<Payment> builder)
+    {
+        builder.ToTable(nameof(Payment));
+        builder.HasKey(p => p.Id);
+
+
+        builder
+            .HasOne(p => p.Booking)
+            .WithOne(b => b.Payment)
+            .HasForeignKey<Payment>(p => p.BookingId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+
+        builder
+            .Property(p => p.Amount)
+            .HasColumnType("decimal")
+            .IsRequired();
+
+        builder
+            .Property(p => p.PaymentDate)
+            .HasColumnType<DateTime>("datetime")
+            .IsRequired();
+
+    }
+}
